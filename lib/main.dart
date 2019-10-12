2514a7e5c354408f7588fe513428c3613c536dc8
Sandeep K K <sandeepkk1507@gmail.com>
2019-10-12 21:06:43 +0530

    added questions.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import './question.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
@@ -37,7 +37,7 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: <Widget>[
-            Text(
+            Question(
               questions[_questionIndex],
             ),
             RaisedButton(
